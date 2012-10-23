efb1452ab21202d8958b7c2a569e4dccb39076b1
i-saint <saint.skr@gmail.com>
2012-10-24 00:18:26 +0900

    DOL_Eval() wip

--- Test/Test3/Test3.cpp
+++ Test/Test3/Test3.cpp
@@ -8,7 +8,7 @@ int main(int argc, char* argv[])
     AllocConsole();
 
     printf(""); // これがないと exe 内に printf() が含まれなくなるので eval の中から呼べなくなる
-    volatile int hoge = 10;
+    volatile int hoge = 10; // 最適化で消えるの防止の volatile 
     DOL_Eval("printf(\"%d\\n\", *(int*)hoge);", "#include <cstdio>\n"); // 現状 eval 内から参照されるホスト側変数は全部 void*
 
     return 0;
