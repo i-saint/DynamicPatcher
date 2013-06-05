a7453b61a3a3e401f05cbe206c52a58f2587be03
i-saint <saint.skr@gmail.com>
2013-06-05 12:25:16 +0900

    不要ファイル削除

--- DynamicPatcherInjector.h
+++
@@ -1,8 +0,0 @@
-﻿// created by i-saint
-// distributed under Creative Commons Attribution (CC BY) license.
-// https://github.com/i-saint/DynamicObjLoader
-
-#include <cstdio>
-#include <string>
-#include <vector>
-
